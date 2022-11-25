77ace0eb7c4cb75d6a62632724c90f3999dea15e
tyzimmer <michael.zimmermann@stud.hslu.ch>
2022-11-25 18:20:50 +0100

    new SGP30

--- Code/sgp30test/sgp30test.ino
+++ Code/sgp30test/sgp30test.ino
@@ -1,4 +1,6 @@
 #include <Wire.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
 #include "Adafruit_SGP30.h"
 
 Adafruit_SGP30 sgp;
@@ -14,7 +16,93 @@ uint32_t getAbsoluteHumidity(float temperature, float humidity) {
     return absoluteHumidityScaled;
 }
 
+const char* ssid = "EEES34-IOT";
+const char* password = "@hsluIOT2019";
+const char* mqtt_server = "192.168.22.77";
+const int mqtt_port = 1883;
+
+WiFiClient wifiClient;
+
+PubSubClient client(wifiClient);
+
+void setup_wifi() {
+    delay(10);
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    randomSeed(micros());
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP32Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      //Once connected, publish an announcement...
+      client.publish("/icircuit/presence/ESP32/", "hello world");
+      // ... and resubscribe
+      client.subscribe("air/clean");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void callback(char* topic, byte *payload, unsigned int length) {
+    Serial.println("-------new message from broker-----");
+    Serial.print("channel:");
+    Serial.println(topic);
+    Serial.print("data:");  
+    Serial.write(payload, length);
+    Serial.println();
+}
+
 void setup() {
+  Serial.begin(115200);
+  Serial.setTimeout(500);// Set time out for 
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+  reconnect();
+}
+
+void publishSerialData(char *serialData){
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.publish("air/clean", serialData);
+}
+void loop() {
+   client.loop();
+   if (Serial.available() > 0) {
+     char mun[501];
+     memset(mun,0, 501);
+     Serial.readBytesUntil( '\n',mun,500);
+     publishSerialData(mun);
+   }
+ }
+
+//>>>>>>>>>>>>>>>>>>>>>>
+
+void setupp() {
   Serial.begin(115200);
   while (!Serial) { delay(10); } // Wait for serial console to open!
 
@@ -34,7 +122,7 @@ void setup() {
 }
 
 int counter = 0;
-void loop() {
+void loopp() {
   // If you have a temperature / humidity sensor, you can set the absolute humidity to enable the humditiy compensation for the air quality signals
   //float temperature = 22.1; // [°C]
   //float humidity = 45.2; // [%RH]
