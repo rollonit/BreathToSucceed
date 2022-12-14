55058790293c07dca958fa6850ea77cf387263ea
rollonit <rohanpradyumna.gundala@stud.hslu.ch>
2022-12-14 14:36:16 +0100

    Got rid of React and added basic Node.js Express server
    This serves the basic files within the public folder at a website on http://localhost:8000/

---
+++ Code/Ground/web/index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const express = require('express')
+
+const app = express();
+const port = 8000;
+
+app.use(express.static('public'))
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
