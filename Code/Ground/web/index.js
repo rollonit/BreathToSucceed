778cb56c9836bac3dd1a2e62492673898e5031f2
rollonit <rohanpradyumna.gundala@stud.hslu.ch>
2022-12-17 21:50:47 +0100

    Add /latest request
    gives the last row in the table

--- Code/Ground/web/index.js
+++ Code/Ground/web/index.js
@@ -1,10 +1,5 @@
-const http = require('http');
 const express = require('express');
 const sqliteJson = require('sqlite-json');
-const { json } = require('express');
-
-let t1 = 1670862897;
-let t2 = 1670862902;
 
 const exporter = sqliteJson('../db/airData.db');
 
@@ -19,6 +14,17 @@ app.use(express.json());
 //Handles API requests here when url is /data
 app.get('/data', (req, res) => {
     exporter.json({ table: 'airQuality' }, (err, json) => {
+        if (err) {
+            console.error(err.message);
+        }
+        //Output the parsed JSON from SQL.
+        res.status(200).json(JSON.parse(json));
+    });
+});
+
+//Handles API requests here when url is /data
+app.get('/latest', (req, res) => {
+    exporter.json('SELECT * FROM airQuality ORDER BY timecode DESC LIMIT 1;', (err, json) => {
         if (err) {
             console.error(err.message);
         }
